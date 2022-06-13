4c678e3d26e2a782cd4e8b71fdd73553cd27dd47
Ben <benodru@gmail.com>
2022-06-13 15:24:56 +0300

    fixed and updated serial reader

---
+++ UnitySerialReader/Assets/Scripts/Sensors.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensors
+{
+    public string SensorName;
+    public string SensorID;
+    public string Time;
+    public string Value;
+}
--- UnitySerialReader/Assets/Scripts/SerialReader.cs
+++ UnitySerialReader/Assets/Scripts/SerialReader.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO.Ports; //to enable reading porst NEEDS .NET 4.X FRAMEWORK
+using System.IO.Ports; //to enable reading ports NEEDS .NET 4.X FRAMEWORK
 
 public class SerialReader : MonoBehaviour
 {
+    List<Sensors> sensors = new List<Sensors>();
     //start serial port and call it stream_data
     //update port for project
-    SerialPort data_stream = new SerialPort("/dev/cu.usbmodem14101", 9600);
+    SerialPort data_stream = new SerialPort("/dev/cu.usbserial-0001", 9600);
     public string receivedString;
+    public string[] dataRows;
     public string[] datas;
+    public string[] port_names = SerialPort.GetPortNames();
 
     // Start is called before the first frame update
     void Start()
     {
+        
         //open the port
+        
+        //for (int i = 0; i < port_names.Length; i++)
+        //{
+        //    Debug.Log(port_names[i]);
+        //}
         data_stream.Open();
     }
 
@@ -23,7 +32,25 @@ public class SerialReader : MonoBehaviour
     {
         receivedString = data_stream.ReadLine();
 
-        string[] datas = receivedString.Split(','); //split the data
-        Debug.Log(datas); //print the data
+        string[] dataRows = receivedString.Split('\n'); //split the data
+
+        for (int i = 0; i == dataRows.Length - 1; i++)
+        {
+            string[] row = dataRows[i].Split(',');
+
+            Sensors s = new Sensors();
+            s.SensorName = row[0];
+            s.SensorID = row[2];
+            s.Time = row[4];
+            s.Value = row[6];
+
+            sensors.Add(s);
+        }
+
+        foreach(Sensors s in sensors)
+        {
+            //string[] datas = receivedString.Split(' '); //split the data
+            Debug.Log(s.SensorName + " " + s.SensorID + " " + s.Value); //print the data
+        }
     }
 }
